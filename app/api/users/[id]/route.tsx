2e6af802e3fd39b879ced0dfe472275ccecd25ea
kunal <contacttokunal@gmail.com>
2023-09-27 13:52:43 +0530

    updating an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -2,10 +2,24 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function GET(
   request: NextRequest,
-  { params: { id } }: { params: { id: number } }
+  { params }: { params: { id: number } }
 ) {
-  if (id > 10)
+  if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
   return NextResponse.json({ id: 1, name: "kunal" });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
 }
