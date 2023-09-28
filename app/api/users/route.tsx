773e62d8b438f533ec332748a55914e604d3c496
kunal <contacttokunal@gmail.com>
2023-09-28 14:29:39 +0530

    Getting data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "kunal" },
-    { id: 2, name: "sonam" },
-  ]);
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
