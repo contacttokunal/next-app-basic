ee875a703ad14fa594c55f4099cb974121316ed2
kunal <contacttokunal@gmail.com>
2023-10-06 01:34:10 +0530

    updating data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -17,16 +17,28 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   const body = await request.json();
   if (!body.name)
     return NextResponse.json({ error: "Name is required" }, { status: 400 });
 
-  if (params.id > 10)
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!user)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-  return NextResponse.json({ id: 1, name: body.name });
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
 export function DELETE(
