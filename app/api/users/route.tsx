a223673fc77c41326042bf2809e987368f8e7540
kunal <contacttokunal@gmail.com>
2023-10-06 01:15:27 +0530

    creating data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -12,5 +12,18 @@ export async function POST(request: NextRequest) {
   if (!body.name)
     return NextResponse.json({ error: "Name is required" }, { status: 400 });
 
-  return NextResponse.json({ id: 3, name: body.name }, { status: 201 });
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (user)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+  return NextResponse.json(newUser, { status: 201 });
 }
