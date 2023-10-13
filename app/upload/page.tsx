0f427dafe5d5ed3403543482a1423436671aabfd
kunal <contacttokunal@gmail.com>
2023-10-13 12:31:57 +0530

    dispalying image and customize widget

---
+++ app/upload/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface cloudinaryResult {
+  public_id: string;
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+  return (
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="A coffee image"
+        />
+      )}
+      <CldUploadWidget
+        uploadPreset="bfkl3dcw"
+        options={{
+          sources: ["local"],
+          multiple: false,
+          maxFiles: 5,
+        }}
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as cloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-success" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;
